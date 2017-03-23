4816e8a3c4a1205b6390647c54419e1aab1f2a21
selvacodes <selvaganapathy@outlook.in>
2017-03-23 13:17:03 +0530

    added callback methods

---
+++ file_callbacks.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+
+function readFile (filename) {
+  fs.readFile(filename, 'utf-8', function (err, data) {
+    let splittedLines = data.split('\n')
+    let outputArray = []
+    for (let i = 0; i <= splittedLines.length; i++) {
+      if (typeof splittedLines[i] == 'string' && splittedLines[i] != '') {
+        if (i % 2 == 0) {
+          outputArray.push(splittedLines[i].toUpperCase())
+        } else {
+          outputArray.push(splittedLines[i].toLowerCase())
+        }
+      }
+    }
+    console.log(outputArray)
+  })
+}
+
+function readTwoFiles (filename1, filename2 , outputFile) {
+  fs.readFile(filename1, 'utf-8', function (err, file1_data) {
+    let splittedLines_one = file1_data.split('\n')
+    let outputArray = []
+    fs.readFile(filename2, 'utf-8', function (err, file2_data) {
+      let splittedLines_two = file1_data.split('\n')
+      for (let i = 0; i <= splittedLines_one.length; i++) {
+        if (typeof splittedLines_one[i] == 'string' && splittedLines_one[i] != '') {
+          if (i % 2 == 0) {
+            outputArray.push(splittedLines_one[i].toUpperCase())
+          } else {
+            outputArray.push(splittedLines_one[i].toLowerCase())
+          }
+        }
+      }
+      for (let i = 0; i <= splittedLines_two.length; i++) {
+        if (typeof splittedLines_two[i] == 'string' && splittedLines_two[i] != '') {
+          if (i % 2 == 0) {
+            outputArray.push(splittedLines_two[i].toUpperCase())
+          } else {
+            outputArray.push(splittedLines_two[i].toLowerCase())
+          }
+        }
+      }
+      fs.writeFile(outputFile, outputArray.join('\n'), 'utf-8', function (err, data) {
+        console.log('Written successfully')
+      })
+      console.log(outputArray)
+    })
+  })
+}
+
+readFile('./sample.txt')
+//readTwoFiles('./sample.txt', './sample2.txt', 'output.txt')
--- file_promises_native.js
+++ file_promises_native.js
@@ -1,54 +1,60 @@
-const fs = require('fs')
 const R = require('ramda')
+const fs = require('fs')
 
+// readFile:: String -> Promise String
 const readFile = function (filename) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filename, 'utf-8', function (err, data) {
-            if (err) {
-                reject(err)
-            }
-            resolve(data)
-        })
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf-8', function (err, data) {
+      if (err) {
+        reject(err)
+      }
+      resolve(data)
     })
+  })
 }
 
+// writeFile :: String -> String -> Promise ()
 const writeFile = filename => data => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(filename, data, 'utf-8', function (err, data) {
-            if (err) {
-                reject(err)
-            }
-            resolve("written successfully")
-        })
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, data, 'utf-8', function (err, data) {
+      if (err) {
+        reject(err)
+      }
+      resolve('Written successfully')
     })
+  })
 }
 
-
+// isEven :: Int -> Bool
 const isEven = num => num % 2 == 0
 
+// lines :: String -> [String]
 const lines = R.split('\n')
 
+// rejectEmptyLines :: [String] -> [String]
 const rejectEmptyLines = R.filter(R.compose(R.not, R.isEmpty))
 
+// changeCaseOfAllElements :: [String] -> [String]
 const changeCaseOfAllElements = arr => arr.map((value, index) => isEven(index) ? value.toUpperCase() : value.toLowerCase())
 
+// joinWithNewLine:: [String] -> String
 const joinWithNewLine = R.join('\n')
 
-// readFile('./sample.txt').then(lines)
-//     .then(rejectEmptyLines)
-//     .then(changeCaseOfAllElements)
-//     .then(joinWithNewLine)
-//     .then(writeFile('./output.txt'))
-//     .then(console.log)
+readFile('./sample.txt').then(lines)
+  .then(rejectEmptyLines)
+  .then(changeCaseOfAllElements)
+  .then(joinWithNewLine)
+  .then(writeFile('./output.txt'))
+  .then(console.log)
 
-// Read two files
-Promise.all([readFile('./sample.txt'), readFile('./sample2.txt')])
-    .then(R.map(lines))
-    //.then(R.converge(R.zip, [R.head, R.last]))
-    //.then(R.tap(console.log))
-    .then(R.flatten)
-    .then(rejectEmptyLines)
-    .then(changeCaseOfAllElements)
-    .then(joinWithNewLine)
-    .then(writeFile('./output.txt'))
-    .then(console.log)
+// Read two fileis
+// Promise.all([readFile('./sample.txt'), readFile('./sample2.txt')])
+//   .then(R.map(lines))
+//   // .then(R.converge(R.zip, [R.head, R.last]))
+//   // .then(R.tap(console.log))
+//   .then(R.flatten)
+//   .then(rejectEmptyLines)
+//   .then(changeCaseOfAllElements)
+//   .then(joinWithNewLine)
+//   .then(writeFile('./output.txt'))
+//   .then(console.log)
